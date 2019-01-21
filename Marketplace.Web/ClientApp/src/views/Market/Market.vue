6bd2ba20eac4da8c08e06b52d429850a1b988972
Halloneo <petrushenko_artu@mail.ru>
2019-01-21 18:18:42 +0300

    = Packages update, small Market page fix

--- Marketplace.Web/ClientApp/src/views/Market/Market.vue
+++ Marketplace.Web/ClientApp/src/views/Market/Market.vue
@@ -1,148 +1,154 @@
 <template>
   <v-container fluid class="pl-0 pb-0 pt-0 pr-0" v-if="market">
-		<v-container class="panel" fluid>
-			<v-flex xs12 class="text-xs-right">
-				<v-btn fab small @click.stop="drawer = !drawer" >
-					<v-icon dark>list</v-icon>
-				</v-btn>
-			</v-flex>
-			<v-navigation-drawer
-				v-model="drawer"
-				absolute
-				temporary
-				right
-			>
-				<v-list class="pa-1">
-					<v-list-tile avatar>
-						<v-list-tile-avatar>
-							<v-icon>settings</v-icon>
-						</v-list-tile-avatar>
-		
-						<v-list-tile-content>
-							<v-list-tile-title>Панель управления</v-list-tile-title>
-						</v-list-tile-content>
-					</v-list-tile>
-				</v-list>
-		
-				<v-list class="pt-0" dense>
-					<v-divider></v-divider>
-					<router-link :to="'/markets/'+market.id+'/createProductOffer'">
-						<v-list-tile @click="">
-							<v-list-tile-action>
-								<v-icon>add</v-icon>
-							</v-list-tile-action>
-			
-							<v-list-tile-content>
-								<v-list-tile-title>Добавить товар</v-list-tile-title>
-							</v-list-tile-content>
-						</v-list-tile>
-					</router-link>
-					<template><edit-market :market="market"></edit-market></template>
-				</v-list>
-			</v-navigation-drawer>
-		</v-container>
-  	<v-container grid-list-xs fluid>
-			<v-flex xs12 
-				class="text-xs-center pt-2 pb-2" 
-				v-if="loading"
-			>
-				<v-progress-circular
-					indeterminate
-					color="primary"
-					:width="7"
-					:size="70"
-				></v-progress-circular>
-			</v-flex>
-			<v-layout row wrap justify-center>
-				<v-flex xs12 sm4 v-if="market">
-					<v-card>
-						<v-card-text class="accent white--text text-xs-center display-2 ">							
-							{{market.name}}
-						</v-card-text>						
-					</v-card>
-				</v-flex>		
-			</v-layout>		
-			<transition-group name="slide" tag="v-layout" class="row wrap mt-5" v-if="market" :key="market.id">
-				<v-flex xs12 sm2 v-for="productOffer in market.productOffers" :key="productOffer.id" class="mr-4 mb-4" style="min-width:290px">	
-					<router-link :to="'/productOffer/' + productOffer.id">					
-						<v-card class="pointer">
-							<v-divider class="accent"></v-divider>											
-							<v-card-text class="text-xs-center">
-								<img :src="productOffer.image" height="200px">
-							</v-card-text>
-							<v-card-text class="headline pb-0">
-								{{productOffer.product.name}}
-							</v-card-text>				
-							<v-card-text class="display-1 pt-0 mt-0">
-								<span class="accent--text">{{productOffer.price | price}} </span><span class="headline">руб.</span>
-							</v-card-text>
-						</v-card>
-					</router-link>				
-				</v-flex>			
-			</transition-group>	
-		</v-container>
-  </v-container fluid class="pl-0 pb-0 pt-0 pr-0">
+    <v-container class="panel" fluid>
+      <v-layout row wrap justify-center align-center>
+        <v-flex xs11 v-if="market">
+          <v-layout justify-center>
+            <v-flex xs1></v-flex>
+            <v-flex xs4>
+              <v-card>
+                <v-card-text class="accent white--text text-xs-center display-2">{{market.name}}</v-card-text>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-flex>
+        <v-flex xs1 class="text-xs-right">
+          <v-btn fab small @click.stop="drawer = !drawer">
+            <v-icon dark>list</v-icon>
+          </v-btn>
+        </v-flex>
+      </v-layout>
+
+      <v-navigation-drawer v-model="drawer" absolute temporary right>
+        <v-list class="pa-1">
+          <v-list-tile avatar>
+            <v-list-tile-avatar>
+              <v-icon>settings</v-icon>
+            </v-list-tile-avatar>
+
+            <v-list-tile-content>
+              <v-list-tile-title>Панель управления</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+
+        <v-list class="pt-0" dense>
+          <v-divider></v-divider>
+          <router-link :to="'/markets/'+market.id+'/createProductOffer'">
+            <v-list-tile @click>
+              <v-list-tile-action>
+                <v-icon>add</v-icon>
+              </v-list-tile-action>
+
+              <v-list-tile-content>
+                <v-list-tile-title>Добавить товар</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </router-link>
+          <template>
+            <edit-market :market="market"></edit-market>
+          </template>
+        </v-list>
+      </v-navigation-drawer>
+    </v-container>
+    <v-container grid-list-xs fluid>
+      <v-flex xs12 class="text-xs-center pt-2 pb-2" v-if="loading">
+        <v-progress-circular indeterminate color="primary" :width="7" :size="70"></v-progress-circular>
+      </v-flex>
+      <transition-group
+        name="slide"
+        tag="v-layout"
+        class="row wrap mt-5"
+        v-if="market"
+        :key="market.id"
+      >
+        <v-flex
+          xs12
+          sm2
+          v-for="productOffer in market.productOffers"
+          :key="productOffer.id"
+          class="mr-4 mb-4"
+          style="min-width:290px"
+        >
+          <router-link :to="'/productOffer/' + productOffer.id">
+            <v-card class="pointer">
+              <v-divider class="accent"></v-divider>
+              <v-card-text class="text-xs-center">
+                <img :src="productOffer.image" height="200px">
+              </v-card-text>
+              <v-card-text class="headline pb-0">{{productOffer.product.name}}</v-card-text>
+              <v-card-text class="display-1 pt-0 mt-0">
+                <span class="accent--text">{{productOffer.price | price}}</span>
+                <span class="headline">руб.</span>
+              </v-card-text>
+            </v-card>
+          </router-link>
+        </v-flex>
+      </transition-group>
+    </v-container>
+  </v-container>
 </template>
 
 <script>
-	export default {
-	props: ['id'],
+export default {
+  props: ["id"],
   data() {
     return {
-			drawer: null
+      drawer: null
+    };
+  },
+  computed: {
+    market() {
+      return this.$store.getters.market;
+    },
+    loading() {
+      return this.$store.getters.loading;
     }
-	},
-	computed: {
-		market () {
-			return this.$store.getters.market
-		},
-		loading () {
-			return this.$store.getters.loading
-		}
-	},
-	created() {		
-		this.$store.dispatch('loadMarket', this.id)
-	},
+  },
+  created() {
+    this.$store.dispatch("loadMarket", this.id);
+  },
   name: "Market"
-}
+};
 </script>
 
 <style scoped>
-	a {
-		text-decoration: none;
-		color: rgba(0,0,0,.87)
-	}
+a {
+  text-decoration: none;
+  color: rgba(0, 0, 0, 0.87);
+}
 
-	.slide-enter {
-			transform: translateY(100px);
-			opacity: 0;
-	}
+.slide-enter {
+  transform: translateY(100px);
+  opacity: 0;
+}
 
-	.slide-enter-active {
-			transition: all 1s ease-out;
-	}
+.slide-enter-active {
+  transition: all 1s ease-out;
+}
 
-	.slide-leave-active {
-			position: absolute;
-	}
+.slide-leave-active {
+  position: absolute;
+}
 
-	.slide-leave-to {
-			transform: translateY(1000px);
-			opacity: 0;
-	}
+.slide-leave-to {
+  transform: translateY(1000px);
+  opacity: 0;
+}
 
-	.slide-move {
-			transition: all 1s;
-	}
-	.pointer {
-		transition: all 0.5s;
-	}
-	.pointer:hover {
-		cursor: pointer;
-		box-shadow: 0px 2px 10px 0px rgba(0,0,0,0.5);		
-	}
-	.panel {
-		box-shadow: inset 0px -2px 0px 0px #039be5, 0px 0px 10px 5px rgba(0,0,0,0.3);
-	}
+.slide-move {
+  transition: all 1s;
+}
+.pointer {
+  transition: all 0.5s;
+}
+.pointer:hover {
+  cursor: pointer;
+  box-shadow: 0px 2px 10px 0px rgba(0, 0, 0, 0.5);
+}
+.panel {
+  box-shadow: inset 0px -2px 0px 0px #039be5,
+    0px 0px 10px 5px rgba(0, 0, 0, 0.3);
+}
 </style>
 
--- Marketplace.Web/ClientApp/src/views/Product/Products.vue
+++ Marketplace.Web/ClientApp/src/views/Product/Products.vue
@@ -1,123 +1,106 @@
 <template>
-  <v-container fluid class="pl-0 pb-0 pt-0 pr-0"> 		
-		<side-menu :items="this.items"></side-menu>
-  	<v-container grid-list-xl fluid>
-			<v-flex xs12 
-				class="text-xs-center pt-2 pb-2" 
-				v-if="loading"
-			>
-				<v-progress-circular
-					indeterminate
-					color="primary"
-					:width="7"
-					:size="70"
-				></v-progress-circular>
-			</v-flex>
-			<v-navigation-drawer
-	      v-model="drawer"
-	      absolute
-	      temporary
-				right
-	    >
-	      <v-list class="pa-1">
-	        <v-list-tile avatar>
-	          <v-list-tile-avatar>
-	            <v-icon>settings</v-icon>
-	          </v-list-tile-avatar>
-	
-	          <v-list-tile-content>
-	            <v-list-tile-title>Панель управления</v-list-tile-title>
-	          </v-list-tile-content>
-	        </v-list-tile>
-	      </v-list>
-	
-	      <v-list class="pt-0" dense>
-	        <v-divider></v-divider>
-	        <v-list-tile
-	          v-for="item in items"
-	          :key="item.title"
-						@click="navigate(item.path)"
-	        >
-						<v-list-tile-action>
-							<v-icon>{{ item.icon }}</v-icon>
-						</v-list-tile-action>
-	
-						<v-list-tile-content>
-							<v-list-tile-title>{{ item.title }}</v-list-tile-title>
-						</v-list-tile-content>
-	        </v-list-tile>
-	      </v-list>
-	    </v-navigation-drawer>
-	
-			<transition-group name="slide" tag="v-layout" class="row wrap justify-center">
-				<v-flex xs12 sm2 v-for="product in products" :key="product.id" style="min-width:290px">
-							<v-card :key="product.id">
-								<v-card-title primary-title class="accent white--text" style="min-height: 70px">
-									{{product.name}}
-									<v-spacer></v-spacer>
-									<v-btn flat icon color="white" @click="deleteProduct(product.id)"><v-icon>delete</v-icon></v-btn>
-								</v-card-title>
-							</v-card>
-				</v-flex>
-			</transition-group>	
-	
-		</v-container>
+  <v-container fluid class="pl-0 pb-0 pt-0 pr-0">
+    <side-menu :items="this.items"></side-menu>
+    <v-container grid-list-xl fluid>
+      <v-flex xs12 class="text-xs-center pt-2 pb-2" v-if="loading">
+        <v-progress-circular indeterminate color="primary" :width="7" :size="70"></v-progress-circular>
+      </v-flex>
+      <v-navigation-drawer v-model="drawer" absolute temporary right>
+        <v-list class="pa-1">
+          <v-list-tile avatar>
+            <v-list-tile-avatar>
+              <v-icon>settings</v-icon>
+            </v-list-tile-avatar>
+
+            <v-list-tile-content>
+              <v-list-tile-title>Панель управления</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+
+        <v-list class="pt-0" dense>
+          <v-divider></v-divider>
+          <v-list-tile v-for="item in items" :key="item.title" @click="navigate(item.path)">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+
+      <transition-group name="slide" tag="v-layout" class="row wrap justify-center">
+        <v-flex xs12 sm2 v-for="product in products" :key="product.id" style="min-width:290px">
+          <v-card :key="product.id">
+            <v-card-title primary-title class="accent white--text" style="min-height: 70px">
+              {{product.name}}
+              <v-spacer></v-spacer>
+              <v-btn flat icon color="white" @click="deleteProduct(product.id)">
+                <v-icon>delete</v-icon>
+              </v-btn>
+            </v-card-title>
+          </v-card>
+        </v-flex>
+      </transition-group>
+    </v-container>
   </v-container>
 </template>
 
 <script>
-	export default {
+export default {
   data() {
     return {
-			drawer: null,	
-			items: [
-				{ title: 'Создать товар', icon: 'add', path: '/products/new' }
-      ]
+      drawer: null,
+      items: [{ title: "Создать товар", icon: "add", path: "/products/new" }]
+    };
+  },
+  computed: {
+    products() {
+      return this.$store.getters.products;
+    },
+    loading() {
+      return this.$store.getters.loading;
     }
-	},
-	computed: {
-		products () {
-			return this.$store.getters.products
-		},loading () {
-			return this.$store.getters.loading
-		}
-	},
-	methods: {
-		deleteProduct (id) {
-			this.$store.dispatch('deleteProduct', id)
-		},
-		navigate(path){
-			this.$router.push(path)
-		}
-	},
-	created () {
-		this.$store.dispatch('loadProducts')
-	},
+  },
+  methods: {
+    deleteProduct(id) {
+      this.$store.dispatch("deleteProduct", id);
+    },
+    navigate(path) {
+      this.$router.push(path);
+    }
+  },
+  created() {
+    this.$store.dispatch("loadProducts");
+  },
   name: "Products"
-}
+};
 </script>
 
 <style scoped>
-	.slide-enter {
-			transform: translateY(100px);
-			opacity: 0;
-	}
+.slide-enter {
+  transform: translateY(100px);
+  opacity: 0;
+}
 
-	.slide-enter-active {
-			transition: all 1s ease-out;
-	}
+.slide-enter-active {
+  transition: all 1s ease-out;
+}
 
-	.slide-leave-active {
-			position: absolute;
-	}
+.slide-leave-active {
+  position: absolute;
+}
 
-	.slide-leave-to {
-			transform: translateY(1000px);
-			opacity: 0;
-	}
+.slide-leave-to {
+  transform: translateY(1000px);
+  opacity: 0;
+}
 
-	.slide-move {
-			transition: all 1s;
-	}
+.slide-move {
+  transition: all 1s;
+}
 </style>
 
